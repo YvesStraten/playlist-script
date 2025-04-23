2cf981bf9bdf8dd593251ecf1080a70ffbd7235e
YvesStraten <yves.straten@gmail.com>
2025-04-23 11:17:50 +0800

    Fixed bug with letter output

--- letter_gen/src/lib.rs
+++ letter_gen/src/lib.rs
@@ -10,18 +10,6 @@ static LETTERS: [&str; 26] = [
     "T", "U", "V", "W", "X", "Y", "Z",
 ];
 
-#[derive(Default)]
-pub struct LetterGenerator {
-    current: usize,
-    current_seq: LetterSequence,
-}
-
-impl LetterGenerator {
-    pub fn new() -> Self {
-        Self::default()
-    }
-}
-
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub struct LetterSequence(pub(crate) String);
 
@@ -39,15 +27,21 @@ impl Deref for LetterSequence {
     }
 }
 
-impl DerefMut for LetterSequence {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-
 impl std::fmt::Display for LetterSequence {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.0)
+    }
+}
+
+#[derive(Default, Debug, Clone)]
+pub struct LetterGenerator {
+    current: usize,
+    current_seq: LetterSequence,
+}
+
+impl LetterGenerator {
+    pub fn new() -> Self {
+        Self::default()
     }
 }
 
@@ -59,7 +53,7 @@ impl Stream for LetterGenerator {
         _cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Option<Self::Item>> {
         let current = self.current;
-        let next_letter = current + 1;
+        let next_letter = if current == 0 { 0 } else {current + 1};
         let current_seq = &*self.current_seq;
         let last_char = &current_seq[current_seq.len() - 1..];
 
@@ -72,7 +66,7 @@ impl Stream for LetterGenerator {
         } else {
             s.pop();
             s.push_str(LETTERS[next_letter]);
-            self.current = next_letter;
+            self.current += 1;
         }
 
         let new_seq = LetterSequence(s);
@@ -83,9 +77,8 @@ impl Stream for LetterGenerator {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use tokio_stream::StreamExt;
-
-    use crate::{LetterGenerator, LetterSequence};
 
     #[tokio::test]
     async fn first_letter_is_a() {
@@ -97,7 +90,12 @@ mod tests {
     #[tokio::test]
     async fn letter_after_last_is_a() {
         let gen = LetterGenerator::new();
-        let letter = gen.skip(25).next().await;
+
+        let mut skipped_iter = gen.skip(25);
+        let letter = skipped_iter.next().await;
         assert_eq!(letter, Some(LetterSequence("ZA".to_string())));
+
+        let letter = skipped_iter.skip(25).next().await; 
+        assert_eq!(letter, Some(LetterSequence("ZZA".to_string())));
     }
 }
